7fbea90300ed77f5d0755438084ff4b7e53732f5
Jackson Oates <jackson.oates@gmail.com>
2015-05-22 08:20:50 -0700

    code format cleanup, moved to rack session cookie configuration

--- app.rb
+++ app.rb
@@ -8,13 +8,16 @@ set :environment, :development
 
 config_file 'config/secrets.yml'
 
+use Rack::Session::Cookie, :key => 'builder_app',
+  :path => '/',
+  :expire_after => 14400, # In seconds
+  :secret => 'secret_stuff'
+
 configure do
-  enable :sessions
   use OmniAuth::Builder do
     provider :twitter, settings.twitter_api_key, settings.twitter_api_secret
   end
 end
-
 
 helpers do
   def current_user
@@ -22,29 +25,23 @@ helpers do
   end
 end
 
-before do
-  # pass if request.path_info =~ /^\/auth\//
-  # redirect to( 'auth/twitter' ) unless current_user
-end
-
 # index, prolly wont get used
 get '/' do
-  haml :index, :format => :html5
+  haml :index, format: :html5
 end
 
 # auth routes
 get '/auth/twitter/callback' do
   session[:uid] = env['omniauth.auth']['uid']
-  redirect to( '/builder' )
+  redirect to('/builder')
 end
 
 get '/auth/failure' do
-    status 503
-    body 'ERROR 503, there was a problem with authentication'
+  status 503
+  body 'ERROR 503, there was a problem with authentication'
 end
 
 # if authenticated, callback will redirect to the form
-
 get '/builder' do
-  haml :builder, :format => :html5
+  haml :builder, format: :html5
 end
